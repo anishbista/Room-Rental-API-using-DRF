e363e7e83ceb11aa0cc91efe1827c26fa5bdf781
anishbista92 <anishbista9235@gmail.com>
2024-03-12 22:31:26 +0545

    Collected static files for deployment

--- rental_management/rental_management/urls.py
+++ rental_management/rental_management/urls.py
@@ -3,17 +3,21 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
 
-urlpatterns = [
-    path(
-        "admin/",
-        include(
-            [
-                path("", include("customadmin.urls")),
-                path("", include("panels.admin_panel.urls")),
-            ]
+urlpatterns = (
+    [
+        path(
+            "admin/",
+            include(
+                [
+                    path("", include("customadmin.urls")),
+                    path("", include("panels.admin_panel.urls")),
+                ]
+            ),
         ),
-    ),
-    path("dj-admin/", admin.site.urls),
-    path("api/user/", include("accounts.urls")),
-    path("api/room/", include("room.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        path("dj-admin/", admin.site.urls),
+        path("api/user/", include("accounts.urls")),
+        path("api/room/", include("room.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
